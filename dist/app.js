4a89c02d409364e2c088728176148017c3210a44
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-03-17 17:10:28 +0200

    Created controllers, models and routes for adding new tasks

--- dist/app.js
+++ dist/app.js
@@ -4,7 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const todos_1 = __importDefault(require("./routes/todos"));
+const body_parser_1 = require("body-parser");
 const app = (0, express_1.default)();
+app.use((0, body_parser_1.json)());
+app.use('/todos', todos_1.default);
 app.use((error, req, res, next) => {
     res.json({ message: error.message });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, {Request, Response, NextFunction} from 'express';
 import todoRoutes from './routes/todos';
-import { error } from 'console';
+import { json } from 'body-parser';
 
 const app = express();
+
+app.use(json());
+
+app.use('/todos', todoRoutes);
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
     res.json({message: error.message});
