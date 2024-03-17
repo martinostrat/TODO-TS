26bb95367b4e2f485bd80d08867f6c66b2170705
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-03-17 17:49:05 +0200

    Added ability to update existing todos

--- dist/controllers/todos.js
+++ dist/controllers/todos.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getTodos = exports.createTodo = void 0;
+exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
 const todo_1 = require("../models/todo");
 const todos = [];
+// Create a new todo item
 const createTodo = (req, res, next) => {
     try {
         const task = req.body.task;
@@ -18,6 +19,7 @@ const createTodo = (req, res, next) => {
     }
 };
 exports.createTodo = createTodo;
+// Lists all todo items
 const getTodos = (req, res, next) => {
     try {
         res.status(201).json({
@@ -29,3 +31,23 @@ const getTodos = (req, res, next) => {
     }
 };
 exports.getTodos = getTodos;
+// Updates existing todo item
+const updateTodo = (req, res, next) => {
+    try {
+        const todoId = req.params.id;
+        const updatedTask = req.body.task;
+        const todoIndex = todos.findIndex(todo => todo.id === todoId);
+        if (todoIndex < 0) {
+            throw new Error('Could not find a todo witch such ID');
+        }
+        todos[todoIndex] = new todo_1.Todo(todos[todoIndex].id, updatedTask);
+        res.status(201).json({
+            message: 'Todo has been updated!',
+            updatedTask: todos[todoIndex]
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+exports.updateTodo = updateTodo;
--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -3,6 +3,7 @@ import { Todo } from "../models/todo";
 
 const todos: Todo[] = [];
 
+// Create a new todo item
 export const createTodo = (req: Request, res: Response, next: NextFunction) => {
     try {
         const task = (req.body as {task: string}).task;
@@ -17,12 +18,36 @@ export const createTodo = (req: Request, res: Response, next: NextFunction) => {
     }
 }
 
+// Lists all todo items
 export const getTodos = (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(201).json({
             tasks: todos
         })
     } catch (error) {
+        console.log(error);
+    }
+}
+
+// Updates existing todo item
+export const updateTodo = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const todoId = req.params.id;
+        const updatedTask = (req.body as {task: string}).task;
+        const todoIndex = todos.findIndex(todo => todo.id === todoId);
+
+        if (todoIndex < 0) {
+            throw new Error('Could not find a todo witch such ID');
+        }
+
+        todos[todoIndex] = new Todo(todos[todoIndex].id, updatedTask);
+
+        res.status(201).json({
+            message: 'Todo has been updated!',
+            updatedTask: todos[todoIndex]
+        })
+        
+    } catch(error) {
         console.log(error);
     }
 }
